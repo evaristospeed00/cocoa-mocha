406fe499cd6d296147a4318cbfb292795050e38b
evaristospeed00 <evaristospeed00@example.com>
2026-04-22 16:01:39 -0600

    Bootstrap storefront config on first backend request

--- backend/src/api/store/public-config/route.ts
+++ backend/src/api/store/public-config/route.ts
@@ -1,22 +1,38 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
 import { ApiKey, Region } from "../../../../.medusa/types/query-entry-points";
+import bootstrapStorefront from "../../../scripts/bootstrap";
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
 
-  const { data: publishableKeys } = await query.graph({
-    entity: "api_key",
-    fields: ["id", "token"],
-    filters: {
-      type: "publishable",
-    },
-  });
+  const getPublishableKeys = async () =>
+    query.graph({
+      entity: "api_key",
+      fields: ["id", "token"],
+      filters: {
+        type: "publishable",
+      },
+    });
 
-  const { data: regions } = await query.graph({
-    entity: "region",
-    fields: ["id", "currency_code"],
-  });
+  const getRegions = async () =>
+    query.graph({
+      entity: "region",
+      fields: ["id", "currency_code"],
+    });
+
+  let { data: publishableKeys } = await getPublishableKeys();
+  let { data: regions } = await getRegions();
+
+  const missingBootstrapData =
+    !((publishableKeys || []) as ApiKey[])[0]?.token ||
+    !((regions || []) as Region[])[0]?.id;
+
+  if (missingBootstrapData) {
+    await bootstrapStorefront({ container: req.scope, args: [] });
+    ({ data: publishableKeys } = await getPublishableKeys());
+    ({ data: regions } = await getRegions());
+  }
 
   const publishableKey = ((publishableKeys || []) as ApiKey[])[0]?.token || "";
   const defaultRegion = ((regions || []) as Region[])[0];
